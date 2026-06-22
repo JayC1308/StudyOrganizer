e29de6659dbaaed17c59b5806aa567bfc834a28b
noahbentele <noahbentele@web.de>
2026-06-22 08:28:38 +0200

    Add files via upload

---
+++ pruefungen.java
@@ -0,0 +1,101 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+public class pruefungen{
+  private int KNr;
+  private int raum;
+  private String pruefer;
+  private int datumTag;
+  private int datumMonat;
+  private int datumJahr;
+  private int anmeldefristTag;
+  private int anmeldefristMonat;
+  private int anmeldefristJahr;  
+  private int uhrzeitH;
+  private int uhrzeitMin;
+  public pruefungen (String m, int r, String p, int dTag, int dMonat, int dJahr, int aTag, int aMonat, int aJahr, int uH, int uMin){
+    KNr = m;
+    raum = r;
+    pruefer = p;
+    datumTag = dTag;
+    datumMonat = dMonat;
+    datumJahr = dJahr;
+    anmeldefristTag = aTag;
+    anmeldefristMonat = aMonat;
+    anmeldefristJahr = aJahr;
+    uhrzeitH = uH;
+    uhrzeitMin = uMin;
+  }
+  public void Kurs(){
+    System.out.println("Kursnummer: " + KNr);
+  }
+  public void Raum(){
+    System.out.println("Raum: " + raum);
+  }
+  public void Pruefer(){
+    System.out.println("Prüfer: " + pruefer);
+  }
+  public void Datum(){
+    System.out.println("Datum: " + datumTag + "." + datumMonat + "." + datumJahr);
+  }
+  public void Uhrzeit(){
+    System.out.println("Uhrzeit: " + uhrzeitH + "." + uhrzeitMin + " Uhr");
+  }
+  public void Anmeldefrist(){
+    if (anmeldefristTag==0||anmeldefristMonat==0) {
+      System.out.println("keine Anmeldefrist");
+    } else {
+      System.out.println("Anmeldefrist bis: " + anmeldefristTag + "."+ anmeldefristMonat + "." + anmeldefristJahr);
+    }
+  }
+  public boolean Schaltjahr() {
+    if ((datumJahr/4)*4==datumJahr) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public void Timer() {
+    LocalDate heute = LocalDate.now();
+    LocalTime jetzt = LocalTime.now();
+    if (heute.getMonthValue()<datumMonat && heute.getDayOfMonth() < datumTag||heute.getYear()<datumJahr&&(heute.getDayOfMonth()<datumTag||heute.getMonthValue+1<datumMonat||heute.getMonthValue-1>datumMonat)||heute.getMonthValue()+1<datumMonat) {
+      System.out.println("Noch über ein Monat bis zur Prüfung");
+    } else {
+      if (heute.getDayOfMonth()!=datumTag) {
+        int d;
+        if (heute.getMonthValue()==datumMonat) {
+          d=datumTag-heute.getDayOfMonth();
+        } 
+        else {
+          if (heute.getMonthValue()==1||heute.getMonthValue()==3||heute.getMonthValue()==5||heute.getMonthValue()==7||heute.getMonthValue()==8||heute.getMonthValue()==10||heute.getMonthValue()==12) {
+            d=datumTag+(31-heute.getDayOfMonth());
+          }
+          else {
+            if (heute.getMonthValue()==4||heute.getMonthValue()==6||heute.getMonthValue()==9||heute.getMonthValue()==11) {
+              d=datumTag+(30-heute.getDayOfMonth());
+            }
+            else {
+              if (Schaltjahr()==false) {
+                d=datumTag+(28-heute.getDayOfMonth());
+              } else {
+                  d=datumTag+(29-heute.getDayOfMonth());
+              }
+            }  
+          } 
+        }       
+        System.out.println("Noch " + d + " Tage bis zur Prüfung");
+      } else {
+        if (heute.getDayOfMonth()==datumTag&&uhrzeitH > jetzt.getHour()) {
+          int s = uhrzeitH - jetzt.getHour();
+          System.out.println("Noch " + s + " Stunden bis zur Prüfung");
+        }else {
+          if (heute.getDayOfMonth()==datumTag && uhrzeitH == jetzt.getHour()) {
+            int m = uhrzeitMin - jetzt.getMinute();
+            System.out.println("Noch " + m + " Minuten bis zur Prüfung");
+          } else {
+            System.out.println("Timer abgelaufen");
+          } 
+        }
+      }
+    } 
+  } 
+}
